86dea623f29ad37cad4e972dc2fd87deb5fb223f
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-09 12:09:38 -0600

    Agregué un GUIManager y un Text del tipo de la carta

---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GUIManager : MonoBehaviour
+{
+    public TMP_Text _name;
+    public TMP_Text _description;
+    public TMP_Text _type;
+    public TMP_Text _attack;
+    public TMP_Text _defense;
+    // Start is called before the first frame update
+    
+}
